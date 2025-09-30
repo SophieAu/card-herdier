8307c837de3afd8d112221d3b45b2d2b35954d0b
Sophie Au <SophieAu@users.noreply.github.com>
2025-09-30 16:39:41 +0200

    Add test run

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { fetchAllPokemon } from "./handler.ts";
 
 // Deploy Test Run
-// fetchAllPokemon();
+fetchAllPokemon();
 
 Deno.cron("Check Pokemon API for new releases", { hour: { exact: 7 } }, {
   backoffSchedule: [60000], // retry after a minute if it failed
