2d90a10d74f3ca292b0677964dcee5fec75a1ffd
Sophie Au <SophieAu@users.noreply.github.com>
2025-09-30 16:20:11 +0200

    Add actual cron job

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { fetchAllPokemon } from "./handler.ts";
 
-// Deno.cron("Check Pokémon API for new releases", { hour: 24 }, {
-//   backoffSchedule: [60000], // retry after a minute if it failed
-// }, fetchAllPokemon);
-
+// Deploy Test Run
 fetchAllPokemon();
+
+Deno.cron("Check Pokémon API for new releases", { hour: 24 }, {
+  backoffSchedule: [60000], // retry after a minute if it failed
+}, fetchAllPokemon);
