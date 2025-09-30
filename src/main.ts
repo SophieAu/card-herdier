7d587727f3399c930989b4167b44a6dc22d8e4ad
Sophie Au <SophieAu@users.noreply.github.com>
2025-09-30 16:34:35 +0200

    Maybe fix cron

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { fetchAllPokemon } from "./handler.ts";
 // Deploy Test Run
 fetchAllPokemon();
 
-Deno.cron("Check Pokemon API for new releases", { hour: 24 }, {
+Deno.cron("Check Pokemon API for new releases", { hour: { exact: 24 } }, {
   backoffSchedule: [60000], // retry after a minute if it failed
 }, fetchAllPokemon);
