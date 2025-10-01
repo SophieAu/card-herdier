fc1bd18e8a4c4f1d9bc3818ee9f17227b13942f9
Sophie Au <SophieAu@users.noreply.github.com>
2025-10-01 13:07:51 +0200

    Remove test run and backup run so I don't double it

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { fetchAllPokemon } from "./handler.ts";
 
-// Deploy Test Run
-fetchAllPokemon();
-
-Deno.cron("Check Pokemon API for new releases", { hour: { exact: 7 } }, {
-  backoffSchedule: [60000], // retry after a minute if it failed
-}, fetchAllPokemon);
+Deno.cron(
+  "Check Pokemon API for new releases",
+  { hour: { exact: 7 } },
+  {},
+  fetchAllPokemon,
+);
