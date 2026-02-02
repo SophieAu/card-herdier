cd6019f2fae3948c5a5b0b6492eb7b275586a455
Sophie Au <SophieAu@users.noreply.github.com>
2026-02-02 19:27:08 +0100

    Pull out email sender

--- src/adapters/email.ts
+++ src/adapters/email.ts
@@ -4,6 +4,7 @@ import { logger } from "./logging.ts";
 const resend = new Resend(Deno.env.get("RESEND_API_KEY"));
 
 const RECIPIENT = Deno.env.get("EMAIL_RECIPIENT");
+const SENDER = "Card Herdier <onboarding@resend.dev>"
 
 export const sendEmail = async (subject: string, body: string) => {
   if (!RECIPIENT) {
@@ -13,7 +14,7 @@ export const sendEmail = async (subject: string, body: string) => {
 
   try {
     await resend.emails.send({
-      from: "Card Herdier <onboarding@resend.dev>",
+      from: SENDER,
       to: [RECIPIENT],
       subject: subject,
       html: body,
