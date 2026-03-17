13f0171db6d386732391d0109394e2edef83a03f
Sophie Au <SophieAu@users.noreply.github.com>
2026-03-17 13:26:36 +0100

    Add axiom logging

--- src/adapters/logging.ts
+++ src/adapters/logging.ts
@@ -1,9 +1,25 @@
 // deno-lint-ignore-file no-explicit-any
 /** biome-ignore-all lint/suspicious/noExplicitAny: just passing along the original console log type */
-const error = (...data: any[]) => console.log(`ERROR: ${data}`, "color: red");
+import { Axiom } from '@axiomhq/js';
 
-const info = (...data: any[]) => console.log(`INFO: ${data}`);
+const AXIOM_DATASET_NAME = "card-herdier"
 
-const warn = (...data: any[]) => console.log(`WARN: ${data}`, "color: orange");
+// biome-ignore lint/style/noNonNullAssertion: we're just praying that this guy is set in the env.
+const axiom = new Axiom({ token: Deno.env.get('AXIOM_TOKEN')! });
+
+const error = (...data: any[]) => {
+    console.log(`ERROR: ${data}`, "color: red")
+    axiom.ingest(AXIOM_DATASET_NAME, [{ level: 'error', message: data.join(' '), timestamp: new Date().toISOString() }]);
+};
+
+const info = (...data: any[]) => {
+    console.log(`INFO: ${data}`)
+    axiom.ingest(AXIOM_DATASET_NAME, [{ level: 'info', message: data.join(' '), timestamp: new Date().toISOString() }]);
+};
+
+const warn = (...data: any[]) => {
+    console.log(`WARN: ${data}`, "color: orange")
+    axiom.ingest(AXIOM_DATASET_NAME, [{ level: 'warn', message: data.join(' '), timestamp: new Date().toISOString() }]);
+};
 
 export const logger = { error, info, warn };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { logger } from "./adapters/logging.ts";
 import { fetchAllPokemon } from "./handler.ts";
 
 Deno.cron(
   "Check Pokemon API for new releases",
   { hour: { exact: 7 } },
   {},
-  fetchAllPokemon,
+  async () => {
+    try {
+      logger.info("Checking Pokemon API for new releases")
+      await fetchAllPokemon();
+    } catch (error) {
+      logger.error("CRON FAILED:", error); // This should at least log
+    }
+  }
 );
