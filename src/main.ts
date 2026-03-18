1e9b4a61e8acadd336788fa771ba92e6324f6f11
Sophie Au <SophieAu@users.noreply.github.com>
2026-03-18 12:52:08 +0100

    Maybe get around cron?

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,27 @@ import { fetchAllPokemon } from "./handler.ts";
 // Add this
 Deno.serve(() => new Response("Card Herdier is running"));
 
-Deno.cron(
-  "Check Pokemon API for new releases",
-  // { hour: { exact: 7 } },
-  { minute: { every: 5 } },
-  {},
-  async () => {
-    console.log(Deno.env.get("DENO_TIMELINE"))
-    console.log(Deno.env.get("IS_PRODUCTION"))
-    if (Deno.env.get("IS_PRODUCTION") !== "true") {
-      logger.info("Skipping non-production cron");
-      return;
-    }
-
-    try {
-      logger.info("Checking Pokemon API for new releases")
-      await fetchAllPokemon();
-    } catch (error) {
-      logger.error("CRON FAILED:", error); // This should at least log
-    }
+(() => {
+  console.log(Deno.env.get("DENO_TIMELINE"))
+  console.log(Deno.env.get("IS_PRODUCTION"))
+  if (Deno.env.get("IS_PRODUCTION") !== "true") {
+    logger.info("Skipping non-production cron");
+    return;
   }
-);
+
+  Deno.cron(
+    "Check Pokemon API for new releases",
+    // { hour: { exact: 7 } },
+    { minute: { every: 5 } },
+    {},
+    async () => {
+
+      try {
+        logger.info("Checking Pokemon API for new releases")
+        await fetchAllPokemon();
+      } catch (error) {
+        logger.error("CRON FAILED:", error); // This should at least log
+      }
+    }
+  );
+})()
