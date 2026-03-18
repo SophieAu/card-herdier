00d6942e4497c4f7ceb18e765e0739d5b4a70f46
Sophie Au <SophieAu@users.noreply.github.com>
2026-03-18 12:29:19 +0100

    Only run cron in prod

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,41 @@ import { fetchAllPokemon } from "./handler.ts";
 // Add this
 Deno.serve(() => new Response("Card Herdier is running"));
 
-Deno.cron(
-  "Check Pokemon API for new releases",
-  // { hour: { exact: 7 } },
-  { minute: { every: 30 } },
-  {},
-  async () => {
-    try {
-      logger.info("Checking Pokemon API for new releases")
-      await fetchAllPokemon();
-    } catch (error) {
-      logger.error("CRON FAILED:", error); // This should at least log
+
+if (Deno.env.get("DENO_TIMELINE") === "production") {
+  Deno.cron(
+    "Check Pokemon API for new releases",
+    // { hour: { exact: 7 } },
+    { minute: { every: 30 } },
+    {},
+    async () => {
+      if (Deno.env.get("DENO_TIMELINE") !== "production") {
+        logger.info("Skipping non-production cron");
+        return;
+      }
+
+      try {
+        logger.info("Checking Pokemon API for new releases")
+        await fetchAllPokemon();
+      } catch (error) {
+        logger.error("CRON FAILED:", error); // This should at least log
+      }
     }
-  }
-);
+  );
+}
+
+
+// Deno.cron(
+//   "Check Pokemon API for new releases",
+//   // { hour: { exact: 7 } },
+//   { minute: { every: 30 } },
+//   {},
+//   async () => {
+//     try {
+//       logger.info("Checking Pokemon API for new releases")
+//       await fetchAllPokemon();
+//     } catch (error) {
+//       logger.error("CRON FAILED:", error); // This should at least log
+//     }
+//   }
+// );
